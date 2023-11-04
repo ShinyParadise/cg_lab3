d64b9e5ae142a709eadfefde1933f4ab3b9281f5
0-w0 <strangeraccoon666@gmail.com>
2023-11-04 15:08:26 +0300

    brezenheim alg before dot class

--- algorithms/brezenheim.py
+++ algorithms/brezenheim.py
@@ -1,2 +1,17 @@
-def brezenheim() -> list:
-    pass
+def brezenheim(beginX, beginY, endX, endY) -> list:
+    points = []
+    x = beginX
+    y = beginY
+    deltaX = (endX - beginX) * 2
+    deltaY = (endY - beginY) * 2
+    delta = - deltaX
+
+    while x <= endX:
+        points.append((int(x), int(y)))
+        x += 1
+        delta += deltaY
+        if delta >= 0:
+            y += 1
+            delta -= deltaX
+
+    return points

b49aa6d10b577a276657ecf410919738f0682df3
Shtepa Anna <80508151+0-w0@users.noreply.github.com>
2023-11-04 15:48:28 +0300

    brezenheim to dot class

--- algorithms/brezenheim.py
+++ algorithms/brezenheim.py
@@ -1,12 +1,15 @@
-def brezenheim(beginX, beginY, endX, endY) -> list:
+from structs.point import Point
+
+def brezenheim(p1: Point, p2: Point) -> list:
+
     points = []
-    x = beginX
-    y = beginY
-    deltaX = (endX - beginX) * 2
-    deltaY = (endY - beginY) * 2
+    x = p1.x
+    y = p1.y
+    deltaX = (p2.x - p1.x) * 2
+    deltaY = (p2.y - p1.y) * 2
     delta = - deltaX
 
-    while x <= endX:
+    while x <= p2.x:
         points.append((int(x), int(y)))
         x += 1
         delta += deltaY
@@ -14,5 +17,4 @@ def brezenheim(beginX, beginY, endX, endY) -> list:
             y += 1
             delta -= deltaX
 
-    points.append((int(endX), int(endY)))
     return points

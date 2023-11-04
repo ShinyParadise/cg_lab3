5f87a1e6a8db4ac0c00d5e6d936ffe2e8297d555
Shtepa Anna <80508151+0-w0@users.noreply.github.com>
2023-11-04 15:25:08 +0300

    Brezenheim add

--- algorithms/brezenheim.py
+++ algorithms/brezenheim.py
@@ -1,2 +1,18 @@
-def brezenheim() -> list:
-    pass
+def brezenheim(beginX, beginY, endX, endY) -> list:
+    points = []
+    x = beginX
+    y = beginY
+    deltaX = (endX - beginX) * 2
+    deltaY = (endY - beginY) * 2
+    delta = - deltaX
+
+    while x <= endX:
+        points.append((int(x), int(y)))
+        x += 1
+        delta += deltaY
+        if delta >= 0:
+            y += 1
+            delta -= deltaX
+
+    points.append((int(endX), int(endY)))
+    return points

99c6aceac7732bc284b7a54caa79a8d685ee937c
Ivan Voronkov <shinodise@gmail.com>
2023-11-04 15:35:24 +0300

    fixed algo

--- algorithms/flood_fill.py
+++ algorithms/flood_fill.py
@@ -1,15 +1,17 @@
+from structs.point import Point
+
 # Списки соседей для каждой точки
 neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
-def flood_fill(start, fill_color, background_color, border_color) -> list:
+def flood_fill(start_point: Point, fill_color, border_color) -> list:
     # Инициализация списка точек для заполнения
     filled_points = []
 
-    flood_fill_recursive(start, fill_color, background_color, border_color, filled_points)
+    flood_fill_recursive(start_point, border_color, filled_points)
     return filled_points
 
 
-def flood_fill_recursive(point, fill_color, background_color, border_color, filled_points):
+def flood_fill_recursive(point: Point, fill_color, border_color, filled_points):
     # Если точка уже заполнена, выходим из рекурсии
     if point in filled_points:
         return
@@ -19,8 +21,8 @@ def flood_fill_recursive(point, fill_color, background_color, border_color, fill
 
     # Проверяем соседей
     for neighbor in neighbors:
-        new_point = (point[0] + neighbor[0], point[1] + neighbor[1])
+        new_point = Point(point.x + neighbor[0], point.y + neighbor[1], fill_color)
 
         # Если соседняя точка не заполнена и не является границей, запускаем рекурсию
-        if new_point not in filled_points and new_point not in border_color:
-            flood_fill_recursive(new_point, fill_color, background_color, border_color, filled_points)
+        if new_point not in filled_points and new_point.color != border_color:
+            flood_fill_recursive(new_point, fill_color, border_color, filled_points)

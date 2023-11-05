5f595f7ed81503dec93717f0479a4a750c90b456
Ivan Voronkov <shinodise@gmail.com>
2023-11-05 18:25:40 +0300

    latest fixes

--- helper_funcs.py
+++ helper_funcs.py
@@ -4,7 +4,13 @@ from structs.point import Point
 def calculate_line_points(color, k: float, x: int, b: int) -> tuple[Point, Point]:
     p1 = Point(0, 0, color)   # начальная точка всегда в (0,0)
 
-    p2x = p1.x + x
-    p2y = k*x + b
-    p2 = Point(p2x, p2y, color)
+    if k > 0:
+        p2x = p1.x + x
+        p2y = k*x + b
+        p2 = Point(p2x, p2y, color)
+    else:
+        p2x = p1.x - x
+        p2y = -k*x + b
+        p2 = Point(p2x, p2y, color)
+    
     return p1,p2
